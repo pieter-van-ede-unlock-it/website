f8baf4cfd85e4e949ba4bbadca777cc1d0db2585
Pieter van Ede <pietervanede@hotmail.com>
2024-09-07 11:55:53 +0200

    begin with services

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,14 @@
+const pluginIcons = require('eleventy-plugin-icons');
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("static/**/*");
+
+    eleventyConfig.addPlugin(pluginIcons, {
+        sources: [
+            {
+                name: 'phosphor-bold',
+                path: 'node_modules/@phosphor-icons/core/assets/bold'
+            }
+        ]
+    });
 };
