090b3f0aebb509a46722f148564bc09599414fc6
Pieter van Ede <pietervanede@hotmail.com>
2024-09-07 22:28:59 +0200

    first part of testimonials done

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const pluginIcons = require('eleventy-plugin-icons');
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("static/**/*");
+    eleventyConfig.addPassthroughCopy({"node_modules/@shoelace-style/shoelace/cdn" : "shoelace"});
 
     eleventyConfig.addPlugin(pluginIcons, {
         sources: [
