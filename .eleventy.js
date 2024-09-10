0d8972a2573c4777d70c4706d315fdca7485052e
Pieter van Ede <pietervanede@hotmail.com>
2024-09-10 20:28:49 +0200

    added normalize for css

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const pluginIcons = require('eleventy-plugin-icons');
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("static/**/*");
     eleventyConfig.addPassthroughCopy({"node_modules/@shoelace-style/shoelace/cdn" : "shoelace"});
+    eleventyConfig.addPassthroughCopy({"node_modules/modern-normalize/modern-normalize.css" : "modern-normalize.css"});
 
     eleventyConfig.addPlugin(pluginIcons, {
         sources: [
